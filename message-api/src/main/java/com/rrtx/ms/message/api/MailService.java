d0f39f901102ebfe3302491363e9d25621b931d9
许国志 <xugz@bjrrtx.com>
2018-07-06 18:14:16 +0800

    commit fix

--- message-api/src/main/java/com/rrtx/ms/message/api/MailService.java
+++ message-api/src/main/java/com/rrtx/ms/message/api/MailService.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient("message")
+@FeignClient(value = "message",fallback = MailServiceFallBack.class)
 public interface MailService {
 
     @RequestMapping("/sendMail")
